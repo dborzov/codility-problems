56491721669ecf8e824a6074b1fa792f7700d940
Dmitry Borzov <peter.borzov@gmail.com>
2015-08-27 15:30:40 +0500

    02.Manhattan: tests for basic shapes

--- 02.manhattan.skyline/sol_test.go
+++ 02.manhattan.skyline/sol_test.go
@@ -1,9 +1,6 @@
 package solution
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestOneLevel(t *testing.T) {
 	exOneLev := []int{10}
@@ -21,10 +18,38 @@ func Test3Levels(t *testing.T) {
 
 }
 
+func TestBump(t *testing.T) {
+	exH := []int{20, 20, 30, 30, 10, 10, 10}
+	if Solution(exH) != 3 {
+		t.Errorf("bump-shaped %v fails", exH)
+	}
+
+}
+
+func TestPyramid(t *testing.T) {
+	exH := []int{20, 20, 30, 30, 20, 20, 20}
+	if Solution(exH) != 2 {
+		t.Errorf("pyramid-shaped %v fails", exH)
+	}
+
+}
+
+func TestPit(t *testing.T) {
+	exH := []int{20, 20, 10, 10, 20, 20, 20}
+	if Solution(exH) != 3 {
+		t.Errorf("pit-shaped %v fails", exH)
+	}
+
+}
+
+func TestEmptyTrimmer(t *testing.T) {
+	tSlice := []int{}
+	tLev := 4
+	tSlice, _ = isIn(tSlice, tLev)
+}
+
 func TestTrimmer(t *testing.T) {
 	tSlice := []int{2, 3, 4, 5}
 	tLev := 4
-	tSlice, res := isIn(tSlice, tLev)
-	fmt.Printf("We get %#v, %#v \n", res, tSlice)
-
+	tSlice, _ = isIn(tSlice, tLev)
 }
