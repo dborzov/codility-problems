18f2eb2f4e67d8780dc1a7944a1580c97bd7b3ff
Dmitry Borzov <peter.borzov@gmail.com>
2015-08-27 15:11:20 +0500

    02.Manhattan: tests for one-item and ladder-shaped Hs

--- 02.manhattan.skyline/sol_test.go
+++ 02.manhattan.skyline/sol_test.go
@@ -5,8 +5,19 @@ import (
 	"testing"
 )
 
-func TestSolution(t *testing.T) {
-	// example := []int{10}
+func TestOneLevel(t *testing.T) {
+	exOneLev := []int{10}
+	if Solution(exOneLev) != 1 {
+		t.Errorf("one-level %v should have one level only", exOneLev)
+	}
+
+}
+
+func Test3Levels(t *testing.T) {
+	exH := []int{10, 20, 30, 40, 50, 60}
+	if Solution(exH) != 6 {
+		t.Errorf("ladder-shaped %v fails", exH)
+	}
 
 }
 
